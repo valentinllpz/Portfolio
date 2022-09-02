63a881212787d5164027b5296094c94aede5c1e5
valentinllpz <lugandlopez.valentin@gmail.com>
2022-09-02 17:32:36 +0200

    project section style done

--- frontend/sections/Projects.tsx
+++ frontend/sections/Projects.tsx
@@ -3,26 +3,18 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { octokit } from "../utils/octokit";
 import { Endpoints } from "@octokit/types";
+import Button from "../components/Button";
 
 type listUserReposResponse = Endpoints["GET /repos/{owner}/{repo}"]["response"];
 
-const RepoCard = ({repo}: {repo : listUserReposResponse["data"]}) => {
+const RepoCard = ({ repo }: { repo: listUserReposResponse["data"] }) => {
   return (
-    <div className="github-repo">
-      <h1 className="font-semibold text-xl dark:text-gray-200 text-gray-700">
-        {repo.name}
-      </h1>
-      <p className="text-base font-normal my-4 text-gray-500">
-        {repo.description}
-      </p>
-      <a
-        href={repo.clone_url}
-        className="font-semibold group flex flex-row space-x-2 w-full items-center"
-      >
+    <div className={styles.repoItem}>
+      <h3 className={styles.h3}>{repo.name}</h3>
+      <p className={styles.repoDescription}>{repo.description}</p>
+      <a href={repo.clone_url} className={styles.repoLink}>
         <p>View Repository </p>
-        <div className="transform  group-hover:translate-x-2 transition duration-300">
-          &rarr;
-        </div>
+        <div>&rarr;</div>
       </a>
     </div>
   );
@@ -52,15 +44,16 @@ const Projects = () => {
     fetchData().catch(console.error);
   }, []);
 
-  console.log(repos);
-
   return (
     <section className={styles.projects}>
-      <h2 className={styles.h2}>Projects</h2>
-		{repos &&
-			repos.map((latestRepo) => (
-				<RepoCard key={latestRepo.id} repo={latestRepo}/>
-			))}
+      <h2 className={styles.h2}>Latest projects</h2>
+      <div className={styles.reposGrid}>
+        {repos &&
+          repos.map((latestRepo) => (
+            <RepoCard key={latestRepo.id} repo={latestRepo} />
+          ))}
+      </div>
+	  <Button text="Visit GitHub"/>
     </section>
   );
 };
